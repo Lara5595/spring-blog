9d33d113c5d685aaa56c099008f0037bc4476d4d
David Lara <david.a.lara05@gmail.com>
2022-11-15 14:30:06 -0600

    missing 1 more question

---
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -0,0 +1,35 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+//@Controller: defines that our class is a controller
+//@GetMapping: defines a method that should be invoked when a GET request is received for the specified URI
+//@ResponseBody: tells Spring that whatever is returned from this method should be the body of our response
+
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/landingPage")
+    @ResponseBody
+    public String landingPage(){
+        return "This is the landing page";
+    }
+
+
+
+//    This adds a number by typing in the url localhost:8080/increment/2
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+
+
+
+}
+
+
---
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -0,0 +1,52 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+
+//    Adding
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public int add(@PathVariable int num1, @PathVariable int num2){
+        return num1 + num2;
+    }
+
+
+// Subtracting
+
+    @GetMapping("/sub/{num3}/and/{num4}")
+    @ResponseBody
+    public int sub(@PathVariable int num3, @PathVariable int num4){
+        return num3 - num4;
+    }
+
+// Multiply
+
+    @GetMapping("/multiply/{num5}/and/{num6}")
+    @ResponseBody
+    public int multiply(@PathVariable int num5, @PathVariable int num6){
+        return num5 * num6;
+    }
+
+
+//    Divide
+    @GetMapping("/divide/{num7}/and/{num8}")
+    @ResponseBody
+    public int divide(@PathVariable int num7, @PathVariable int num8){
+        return num7 / num8;
+    }
+
+
+
+}
+
+
+
+
+
