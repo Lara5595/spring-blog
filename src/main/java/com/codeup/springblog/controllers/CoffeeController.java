c9431b30359ce9571012aae44836a4868564d37a
David Lara <david.a.lara05@gmail.com>
2022-11-16 14:56:16 -0600

    use post and new cons

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,8 +1,12 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Coffee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/coffee")
@@ -15,8 +19,17 @@ public class CoffeeController {
 
     @GetMapping("/{roast}")
     public String roast(@PathVariable String roast, Model model){
+//        Generating and empty coffee ojbject
+        Coffee selection = new Coffee(roast, "Cool Beans");
+        Coffee selection2 = new Coffee(roast, "Coffee Bros");
+//        Using the coffee setter
+        selection.setRoast(roast);;
+//        Using origin from coffee
+        selection.setOrigin("Ethiopia");
+        selection2.setOrigin("Vietnam");
+        List<Coffee> selections = new ArrayList<>(List.of(selection,selection2));
 //        Model makes the information available to the view
-        model.addAttribute("roast", roast);
+        model.addAttribute("selections", selections);
         return "coffee";
     }
 
