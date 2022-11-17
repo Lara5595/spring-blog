a1b70296268ef4abe5dfeeb9c77c3bada2456114
David Lara <david.a.lara05@gmail.com>
2022-11-17 15:08:29 -0600

    Save

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,16 +1,25 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Coffee;
+import com.codeup.springblog.repositories.CoffeeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 @Controller
 @RequestMapping("/coffee")
 public class CoffeeController {
+
+//    We are creating a coffee repo and constructor from the coffee repo instance class
+    private final CoffeeRepository coffeeDao;
+
+    public CoffeeController(CoffeeRepository coffeeDao){
+        this.coffeeDao = coffeeDao;
+    }
 
     @GetMapping
     public String coffee(){
@@ -32,6 +41,28 @@ public class CoffeeController {
         model.addAttribute("selections", selections);
         return "coffee";
     }
+
+    // This creates a new page that shows the form
+    @GetMapping("/new")
+    public String addCoffeeForm(){
+        return "create-coffee";
+    }
+//    This is going to captures the parameters that are being send from the form
+    @PostMapping("/new")
+    public String addCoffee(@RequestParam(name = "roast") String roast, @RequestParam(name = "origin")String origin, @RequestParam(name = "brand")String brand){
+        Coffee coffee = new Coffee(roast,origin,brand);
+        coffeeDao.save(coffee);
+        return "redirect:/coffee/all-coffees";
+    }
+
+//    This gets all our coffees for all coffees.html
+    @GetMapping("/all-coffees")
+    public String allCoffees(Model model){
+        List<Coffee> coffees = coffeeDao.findAll();
+        model.addAttribute("coffees", coffees);
+        return "all-coffees";
+    }
+
 
 
     @PostMapping
--- src/main/java/com/codeup/springblog/models/Coffee.java
+++ src/main/java/com/codeup/springblog/models/Coffee.java
@@ -1,26 +1,26 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
 // (POJO)Plain Ole Java Object
+@Entity
+@Table(name="coffees")
 public class Coffee {
+//    this id is for the entity
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    //    We are creating the columns
+    @Column(nullable = false, length = 50)
 //     Instances
     private String roast;
-    private String origin;
 
+    @Column(nullable = false)
+    private String origin;
+    @Column(nullable = false, length = 100)
     private String brand;
 
-
-//    Constructors
-    public Coffee() {
-    }
-
-    public Coffee(String brand) {
-        this.brand = brand;
-    }
-
-    public Coffee(String roast,String brand) {
-        this.roast = roast;
-        this.brand = brand;
-    }
 
 //    Getters and Setters
     public String getRoast() {
@@ -46,4 +46,41 @@ public class Coffee {
     public void setBrand(String brand) {
         this.brand = brand;
     }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+
+    //    Constructors
+    public Coffee() {
+    }
+
+    public Coffee(String brand) {
+        this.brand = brand;
+    }
+
+    public Coffee(String roast,String brand) {
+        this.roast = roast;
+        this.brand = brand;
+    }
+
+
+    public Coffee(String roast, String origin, String brand) {
+        this.roast = roast;
+        this.origin = origin;
+        this.brand = brand;
+    }
 }
+
+
+//create a new branch call repositories-and-jpa with git checkout -b repositories-and-jpa
+
+
+
