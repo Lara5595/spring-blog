fc069c21142587c180bdced496ba62d4789de58d
David Lara <david.a.lara05@gmail.com>
2022-11-18 10:46:12 -0600

    created post dir

---
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -0,0 +1,28 @@
+package com.codeup.springblog.models;
+
+public class Post {
+//  Instances
+
+    private String title;
+
+    private String body;
+
+// Getters and Setters
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
