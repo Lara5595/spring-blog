f1e4caf14fa903e44895433324d2db2b2c2e73ad
David Lara <david.a.lara05@gmail.com>
2022-12-06 15:03:37 -0600

    added crud

--- src/test/java/com/codeup/springblog/UserIntegrationTests.java
+++ src/test/java/com/codeup/springblog/UserIntegrationTests.java
@@ -1,5 +1,6 @@
 package com.codeup.springblog;
 
+import com.codeup.springblog.models.Post;
 import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.PostRepository;
 import com.codeup.springblog.repositories.UserRepository;
@@ -10,16 +11,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 import javax.servlet.http.HttpSession;
 
-import static junit.framework.TestCase.assertNotNull;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringBlogApplication.class)
@@ -79,6 +83,103 @@ public class UserIntegrationTests {
         // It makes sure the returned session is not null
         assertNotNull(httpSession);
     }
+
+
+//    CREATE
+//    creates test
+    @Test
+    public void testCreatePost() throws Exception {
+        // Makes a Post request to /ads/create and expect a redirection to the Ad
+        this.mvc.perform(
+                        post("/posts/new").with(csrf())
+                                .session((MockHttpSession) httpSession)
+                                // Add all the required parameters to your request like this
+                                .param("title", "This is the test title")
+                                .param("body", "This is the test body"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+
+
+//    READ
+// show test
+
+    @Test
+    public void testShowPost() throws Exception {
+
+        Post existingAd = postDao.findAll().get(17);
+
+        // Makes a Get request to /ads/{id} and expect a redirection to the Ad show page
+        this.mvc.perform(get("/posts/" + existingAd.getId()))
+                .andExpect(status().isOk())
+                // Test the dynamic content of the page
+                .andExpect(content().string(containsString(existingAd.getBody())));
+    }
+
+    @Test
+    public void testAdsIndex() throws Exception {
+        Post existingAd = postDao.findAll().get(0);
+
+        // Makes a Get request to /ads and verifies that we get some static text of the ads/index.html template and at least the title from the first Ad is present in the template.
+        this.mvc.perform(get("posts/create/all-posts"))
+                .andExpect(status().isOk())
+                // Test the static content of the page
+                .andExpect(content().string(containsString("Latest ads")))
+                // Test the dynamic content of the page
+                .andExpect(content().string(containsString(existingAd.getTitle())));
+    }
+
+
+
+// UPDATE
+// edit test
+    @Test
+    public void testEditPost() throws Exception {
+        // Gets the first Ad for tests purposes
+        Post existingAd = postDao.findAll().get(0);
+
+        // Makes a Post request to /ads/{id}/edit and expect a redirection to the Ad show page
+        this.mvc.perform(
+                        post("/posts/" + existingAd.getId() + "/edit").with(csrf())
+                                .session((MockHttpSession) httpSession)
+                                .param("title", "edited title")
+                                .param("body", "edited description"))
+                .andExpect(status().is3xxRedirection());
+
+        // Makes a GET request to /ads/{id} and expect a redirection to the Ad show page
+        this.mvc.perform(get("/posts/" + existingAd.getId()))
+                .andExpect(status().isOk())
+                // Test the dynamic content of the page
+                .andExpect(content().string(containsString("edited title")))
+                .andExpect(content().string(containsString("edited body")));
+    }
+
+
+
+//    DELETE
+//    This deletes the post
+
+
+//    @Test
+//    public void testDeleteAd() throws Exception {
+//        // Creates a test Ad to be deleted
+//        this.mvc.perform(
+//                        post("/ads/create").with(csrf())
+//                                .session((MockHttpSession) httpSession)
+//                                .param("title", "ad to be deleted")
+//                                .param("body", "won't last long"))
+//                .andExpect(status().is3xxRedirection());
+//
+//        // Get the recent Ad that matches the title
+//        Post existingAd = postDao.findByTitle("ad to be deleted");
+//
+//        // Makes a Post request to /ads/{id}/delete and expect a redirection to the Ads index
+//        this.mvc.perform(
+//                        post("/posts/" + existingAd.getId() + "/delete").with(csrf())
+//                                .session((MockHttpSession) httpSession)
+//                                .param("id", String.valueOf(existingAd.getId())))
+//                .andExpect(status().is3xxRedirection());
+//    }
 
 
 
